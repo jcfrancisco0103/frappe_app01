a6f764b1d41ced8b1a0c95f4173a55967d3e568b
jcfrancisco0103 <152581275+jcfrancisco0103@users.noreply.github.com>
2024-10-10 06:42:56 +0000

    Create New DocType

---
+++ my_app01/jc_francisco/doctype/my_test_doctype/my_test_doctype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Jan Christopher Francisco and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("My Test DocType", {
+// 	refresh(frm) {
+
+// 	},
+// });
